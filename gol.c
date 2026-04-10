a79d308f78084f659d54030c4bc46256f30626e9
FoyerD <amitfoye@post.bgu.ac.il>
2026-04-10 10:23:28 +0300

    added draw function

--- gol.c
+++ gol.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 #define WIDTH 100
 #define LENGTH 100
@@ -7,23 +8,33 @@ struct grid {
     short matrix[LENGTH][WIDTH];
 };
 
-void init_grid(struct grid grid);
-void draw_grid(struct grid grid);
-void update_grid(struct grid grid);
+void init_grid(struct grid* grid);
+void draw_grid(struct grid* grid);
+void update_grid(struct grid* grid);
 
 int main(int argc, char** argv) {
     struct grid grid;
-    init_grid(grid);
-
+    init_grid(&grid);
+    draw_grid(&grid);
     return 0;
 }
 
 
 
-void init_grid(struct grid grid) {
+void init_grid(struct grid* grid) {
     for (int row = 0; row < LENGTH; row++) {
         for (int col = 0; col < WIDTH; col++) {
-            grid.matrix[row][col] = (row + col) % 2 == 0;
+            grid->matrix[row][col] = (row + col) % 2 == 0;
         }
+    }
+}
+
+
+void draw_grid(struct grid* grid) {
+    for (int row = 0; row < LENGTH; row++) {
+        for (int col = 0; col < WIDTH; col++) {
+            printf("%c", grid->matrix[row][col] ? '*' : ' ');
+        }
+        printf("\n");
     }
 }
