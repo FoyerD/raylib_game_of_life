51d162a8af1f2e6391432ed4e2d69913030af5bb
FoyerD <amitfoye@post.bgu.ac.il>
2026-04-10 10:18:38 +0300

    added initialize function

--- gol.c
+++ gol.c
@@ -1,7 +1,29 @@
-#include <stdlib>
+#include <stdlib.h>
 
-# define
+#define WIDTH 100
+#define LENGTH 100
 
 struct grid {
-
+    short matrix[LENGTH][WIDTH];
 };
+
+void init_grid(struct grid grid);
+void draw_grid(struct grid grid);
+void update_grid(struct grid grid);
+
+int main(int argc, char** argv) {
+    struct grid grid;
+    init_grid(grid);
+
+    return 0;
+}
+
+
+
+void init_grid(struct grid grid) {
+    for (int row = 0; row < LENGTH; row++) {
+        for (int col = 0; col < WIDTH; col++) {
+            grid.matrix[row][col] = (row + col) % 2 == 0;
+        }
+    }
+}
